1af5f69dfb7f3bede54385a7bf21217e55f020dd
Frederik <frederik.em.ruprecht@gmail.com>
2022-07-11 15:04:02 +0200

    Final TemperatureMeasurement

--- TempMessungKlasse.ino
+++ TempMessungKlasse.ino
@@ -1,84 +1,84 @@
-#include <OneWire.h>
+#include <OneWire.h>          
 #include <DallasTemperature.h>
-
-
 #define temPin 2
+
+float x=0;
+float c=1000;
+float y=0;
+int v=0;
+float b=0;
 
 OneWire oneWire(temPin); 
 
 DallasTemperature sensors(&oneWire);
- 
- class TempMessung
-{  
-  public: 
-float durchschnitt(int AnzMessung)
+
+class TemperaturMessung
 {
-  float sum = 0;
-  for (int x = 0; x <AnzMessung; ++x)
-  {
-  sensors.requestTemperatures();
-  sum += sensors.getTempCByIndex(0);
-  delay(1);
-  }
-  float Ave = sum / AnzMessung;
-  return Ave;
-}
+ public:
+        float Max_Wert(float Messwert)
+        { 
+          float y=Messwert;
+          x=max(x,y);
+          return x;
+        }
 
-float maxWert (int AnzMessung)
- {
-  float y = 0;
-  for (int x = 0; x<AnzMessung; ++x)
-  {
-  sensors.requestTemperatures();
-  float z = sensors.getTempCByIndex(0);
-  y = max(y,z);
-  delay(1);}
-  return y;
- }
+         float Min_Wert(float Messwert)
+        { 
+          float y=Messwert;
+          c=min(c,y);
+          return c;
+        }
 
-float minWert (int AnzMessung)
- {
-  sensors.requestTemperatures();
-  float y = sensors.getTempCByIndex(0);
-  for (int x = 0; x<AnzMessung; ++x)
-  {
-  sensors.requestTemperatures();
-  float z = sensors.getTempCByIndex(0);
-  y = min(y,z);
-  delay(1);}
-  return y;
- }
+        float Durchschnitt(float Messung, int Anzahl){
+          y = Messung;
+          float average= y/Anzahl;
+          return average;
+        }
+
+        void addSample(float Sample){
+          y=Sample;
+        }
+
 };
-TempMessung A1;
 
+TemperaturMessung A1;
 void setup()
 {
   Serial.begin(9600);
   sensors.begin();
-         
 }
-  
+
 void loop() 
 {
-int AnzahlMessung = 100;
+  v++;
+  sensors.requestTemperatures();
+  float y = sensors.getTempCByIndex(0);
+  b+=y;
+  Serial.println("Maximaler Wert");
+  Serial.println(A1.Max_Wert(y));
+  
+  Serial.println("Minimaler Wert");
+  Serial.println(A1.Min_Wert(y));
+  
+  Serial.println("Durchschnittlicher Wert");
+  Serial.println(A1.Durchschnitt(b,v));
+  Serial.println(" ");
 
+  A1.addSample(35.32);
 
-sensors.requestTemperatures();
-Serial.print("Erste Messung: ");
-Serial.println(sensors.getTempCByIndex(0));
-
-Serial.print("Gerundete Messung aus ");
-Serial.print(AnzahlMessung);
-Serial.print(" Messungen: ");
-Serial.println(A1.durchschnitt(AnzahlMessung));
-
-Serial.print("Höchste Messung aus ");
-Serial.print(AnzahlMessung);
-Serial.print(" Messungen: ");
-Serial.println(A1.maxWert(AnzahlMessung));
-
-Serial.print("Niedrigste Messung aus ");
-Serial.print(AnzahlMessung);
-Serial.print(" Messungen: ");
-Serial.println(A1.minWert(AnzahlMessung));
+  if (Serial.available())
+  {
+    char data = Serial.read();
+    if (data == 'o')
+    {
+      x=0;
+      c=1000;
+      b=0;
+      v=0;
+      Serial.println("Resetted");
+    }
+    else{
+      Serial.println("Not Resetted");
+    }
+  }
 }
